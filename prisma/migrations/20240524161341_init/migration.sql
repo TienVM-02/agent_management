3360619376f27accedcedc28ba0f03d6598bc5d1
TienK14 <vominhtienyahoo@gmail.com>
2024-05-24 23:32:02 +0700

    [setup] setup prisma with mysql

---
+++ prisma/migrations/20240524161341_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Roles` (
+    `ID` VARCHAR(191) NOT NULL,
+    `role` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`ID`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
